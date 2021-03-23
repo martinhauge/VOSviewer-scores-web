c62a1b690d865731c4ea9866c043ab792e7e05f5
Martin Hauge <maha@kb.dk>
2021-03-23 10:01:55 +0100

    Revert "Secret app key changed from static to random and dynamic."
    This reverts commit c956613cf8d44bfe86dd378e2baddab87d9c7f9a.
    
    Resolve unmerged changes.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, flash
 from logic.scores import ScoresHandler
-import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(16)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
